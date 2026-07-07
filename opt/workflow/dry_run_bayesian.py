d02363e28ad88e65f3944a80fbe76becb9dfb228
Leo-kp <leandropceron@gmail.com>
2026-07-07 10:03:10 +0200

    update bayesian protocol

--- opt/workflow/dry_run_bayesian.py
+++ opt/workflow/dry_run_bayesian.py
@@ -45,84 +45,38 @@ def run_dry_opt(cwd=cwd):
     else: 
         print("X are not dataframe with column names")
 
-    # pjack_data= [point[0] for point in x_filtered]
-    # wr_data= [point[1] for point in x_filtered]
+    y_pure_floats = [float(score) for score in y_init]
 
-    # pjack_min, pjack_max= min(pjack_data), max(pjack_data)
-    # wr_min, wr_max= min(wr_data), max(wr_data)
+    pjack_data= [point[0] for point in x_filtered]
+    wr_data= [point[1] for point in x_filtered]
 
-    # pjack_padding=(pjack_max - pjack_min)*0.01
-    # wr_padding= (wr_max - wr_min)*0.01
+    pjack_min, pjack_max= min(pjack_data), max(pjack_data)
+    wr_min, wr_max= min(wr_data), max(wr_data)
 
-    # search_space=[ #to avoid point outside search space
-    #     Real(pjack_min-pjack_padding, pjack_max+pjack_padding,name='pjack'),
-    #     Real(wr_min-wr_padding, wr_max+wr_padding,name='wr')
-    # ]
+    pjack_padding=(pjack_max - pjack_min)*0.01
+    wr_padding= (wr_max - wr_min)*0.01
 
-    # optimizer= Optimizer(
-    #     dimensions=search_space,
-    #     base_estimator="GP",
-    #     acq_func="EI",
-    #     random_state=42
-    # )
+    search_space=[ #to avoid point outside search space
+        Real(pjack_min-pjack_padding, pjack_max+pjack_padding,name='pjack'),
+        Real(wr_min-wr_padding, wr_max+wr_padding,name='wr')
+    ]
 
-    # print(f"Training surrogate model 2D space (pjack,wr)...")
-    # optimizer.tell(x_filtered,y_init,fit=True)
-    # print("optimizer ready")
+    optimizer= Optimizer(
+        dimensions=search_space,
+        base_estimator="GP",
+        acq_func="EI",
+        random_state=42
+    )
 
-    # next_point=optimizer.ask()
-    # print(f"\nrecomended experiment")
-    # print(f"suggested pjack: {next_point[0]:.4f}")
-    # print(f"suggested wr: {next_point[1]:.4f}")
+    print(f"Training surrogate model 2D space (pjack,wr)...")
+    optimizer.tell(x_filtered,y_pure_floats,fit=True)
+    print("optimizer ready")
 
-    #1
-    # min_idx =int(np.argmin(y_init))
-    # max_idx =int(np.argmax(y_init))
+    next_point=optimizer.ask()
+    print(f"\nrecomended experiment")
+    print(f"suggested pjack: {next_point[0]:.4f}")
+    print(f"suggested wr: {next_point[1]:.4f}")
 
-    # print(f"\nBest run check")
-    # print(f"run index: {min_idx}")
-    # print(f"input par: {x_.iloc[min_idx]}")
-    # print(f"Mismatch y par: {y_init[min_idx]:.4f}")
-
-    # print(f"\nWorst run check")
-    # print(f"run index: {max_idx}")
-    # print(f"input par: {x_.iloc[max_idx]}")
-    # print(f"Mismatch y par: {y_init[max_idx]:.4f}")
-
-    # print("random verification")
-    # random_idx=random.randint(0,len(y_init)-1)
-    # print(f"selected random idx verification: {random_idx}")
-    # raw_sample_dir= y_list[random_idx]
-
-    # manual_cost=objective_function(raw_sample_dir,fieldata)
-    # loop_cost=y_init[random_idx]
-
-    # print(f"loop-calculated mistmatch: {loop_cost}")
-    # print(f"Manual calculated Mistmatch: {manual_cost}")
-
-    #0
-    # print("verify x")
-    # print(f"Type: {type(x_)}")
-    # print(f"Dimensions: {x_.shape}")
-    # print(f"Columns: {list(x_.columns)}")
-    # print(f"Missing/NaN: {x_.isnull().sum().sum()}")
-    # print(x_.head(3))
-
-    # print("verify y")
-    # print(f"Original Type: {type(y_list)}")
-    # print(f"List Length: {len(y_list)}")
-    # y_array=np.array(y_list)
-
-    # print(f"converted shape: {y_array.shape}")
-    # print(f"data type: {y_array.dtype}")
-
-    # if y_array.ndim>1:
-    #     print(f"target dim: {y_array.ndim}")
-    #     print(f"Shape of element 0: {y_array[0].shape}")
-    # else:
-    #     print(f"target dim: 1D flat vector, perfect for optimizer")
-
-    # print(y_array[0])
 if __name__=="__main__":
     run_dry_opt(cwd)
 
